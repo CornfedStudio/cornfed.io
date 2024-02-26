52b254993db71e94307f178fd3d5a48c092b4eb0
Michael Boland <michael@boland.co>
2024-02-25 21:21:29 -0600

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: 'Cornfed Studio',
+  url: 'https://cornfed.io',
+  ogImage: 'https://cornfed.io/og.jpg',
+  description:
+    "A hearty web3 studio - front-ends, back-ends, smart contracts, and more. We're a small Midwestern team of developers and designers building in crypto.",
+  links: {
+    github: 'https://github.com/cornfedstudio'
+  }
+}
+
+export type SiteConfig = typeof siteConfig
